e7b21541d5431e5b6c4c640e451c9f7078c794c4
Igor <ermygut@gmail.com>
2021-08-09 18:21:26 +0300

    basic bootstrap validation

--- app.js
+++ app.js
@@ -58,11 +58,7 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 
 app.put('/campgrounds/:id', async (req, res) => {
 	const { id } = req.params;
-	const campground = await Campground.findByIdAndUpdate(
-		id,
-		{ ...req.body.campground },
-		{ new: true }
-	);
+	const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
 	res.redirect(`/campgrounds/${campground._id}`);
 });
 
