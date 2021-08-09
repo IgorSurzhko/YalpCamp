d75fc6e849d04f6e69a1ba0355e8d55f8ad6bba7
Igor <ermygut@gmail.com>
2021-08-09 23:31:56 +0300

    JOI Schema Validations

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const Joi = require('joi');
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
@@ -56,6 +57,22 @@ app.get(
 app.post(
 	'/campgrounds',
 	catchAsync(async (req, res, next) => {
+		//Joi validation
+		const campgroundSchema = Joi.object({
+			campground: Joi.object({
+				title: Joi.string().required(),
+				price: Joi.number().required().min(0),
+				image: Joi.string().required(),
+				location: Joi.string().required(),
+				description: Joi.string().required()
+			}).required()
+		});
+		const { error } = campgroundSchema.validate(req.body);
+		if (error) {
+			const msg = error.details.map(el => el.message).join(',');
+			throw new ExpressError(msg, 400);
+		}
+		console.log(result);
 		const campground = new Campground(req.body.campground); // as usual we accept here an object, so body is an object
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
