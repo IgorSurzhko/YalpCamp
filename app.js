02147b3c054e6162ac1f3ab1b127dc3aba6743ca
Igor <ermygut@gmail.com>
2021-08-11 14:06:30 +0300

    reviews Joi valid + CRUD

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const { campgroundSchema } = require('./schemas');
+const { campgroundSchema, reviewSchema } = require('./schemas');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
@@ -41,6 +42,16 @@ const validateCampground = (req, res, next) => {
 	}
 };
 
+const validateReview = (req, res, next) => {
+	const { error } = reviewSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map(el => el.message).join(',');
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
+
 app.get('/', (req, res) => {
 	res.render('home');
 });
@@ -60,7 +71,7 @@ app.get('/campgrounds/new', (req, res) => {
 app.get(
 	'/campgrounds/:id',
 	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
+		const campground = await Campground.findById(req.params.id).populate('reviews');
 		res.render('campgrounds/show', { campground });
 	})
 );
@@ -99,6 +110,30 @@ app.delete(
 		const { id } = req.params;
 		const campground = await Campground.findByIdAndDelete(id);
 		res.redirect(`/campgrounds`);
+	})
+);
+
+app.post(
+	'/campgrounds/:id/reviews',
+	validateReview,
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		await review.save();
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
+
+app.delete(
+	'/campgrounds/:id/reviews/:reviewId',
+	catchAsync(async (req, res) => {
+		const { id, reviewId } = req.params;
+		//$pull operator PULLS out of array of @reviews@ the document with @reviewId@
+		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+		await Review.findByIdAndDelete(reviewId);
+		res.redirect(`/campgrounds/${id}`);
 	})
 );
 
