64b459791375101699b257d1dd9b065ff0fc8fd2
Igor <ermygut@gmail.com>
2021-08-12 14:50:28 +0300

    Configuring Session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
+const session = require('express-session');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -31,6 +32,18 @@ mongoose.set('useFindAndModify', false); //avoid deprecation warnings
 app.use(express.urlencoded({ extended: true })); //parses data from HTML FORMS
 app.use(methodOverride('_method')); //prefix for method-override URL
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+	secret: 'thisshouldbeabettersecret!',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true, //security flag
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7
+	}
+};
+app.use(session(sessionConfig));
 
 app.use('/campgrounds', campgrounds);
 //to have access to :id we use  (mergeParams: true) inside router file
