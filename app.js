99dcef85cbfb29202bbfb4500b2a6fc00a4e0bd1
Igor <ermygut@gmail.com>
2021-08-12 14:23:20 +0300

    refactoring file structure

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const { campgroundSchema, reviewSchema } = require('./schemas');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 const ejsMate = require('ejs-mate');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -31,111 +30,13 @@ mongoose.set('useFindAndModify', false); //avoid deprecation warnings
 app.use(express.urlencoded({ extended: true })); //parses data from HTML FORMS
 app.use(methodOverride('_method')); //prefix for method-override URL
 
-//Joi validation
-const validateCampground = (req, res, next) => {
-	const { error } = campgroundSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map(el => el.message).join(',');
-		throw new ExpressError(msg, 400);
-	} else {
-		next();
-	}
-};
-
-const validateReview = (req, res, next) => {
-	const { error } = reviewSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map(el => el.message).join(',');
-		throw new ExpressError(msg, 400);
-	} else {
-		next();
-	}
-};
+app.use('/campgrounds', campgrounds);
+//to have access to :id we use  (mergeParams: true) inside router file
+app.use('/campgrounds/:id/reviews', reviews);
 
 app.get('/', (req, res) => {
 	res.render('home');
 });
-
-app.get(
-	'/campgrounds',
-	catchAsync(async (req, res) => {
-		const campgrounds = await Campground.find({}); //find\show all docs in db
-		res.render('campgrounds/index', { campgrounds });
-	})
-);
-
-app.get('/campgrounds/new', (req, res) => {
-	res.render('campgrounds/new');
-});
-
-app.get(
-	'/campgrounds/:id',
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id).populate('reviews');
-		res.render('campgrounds/show', { campground });
-	})
-);
-
-app.post(
-	'/campgrounds',
-	validateCampground,
-	catchAsync(async (req, res, next) => {
-		const campground = new Campground(req.body.campground); // as usual we accept here an object, so body is an object
-		await campground.save();
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-
-app.get(
-	'/campgrounds/:id/edit',
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
-		res.render('campgrounds/edit', { campground });
-	})
-);
-
-app.put(
-	'/campgrounds/:id',
-	validateCampground,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-
-app.delete(
-	'/campgrounds/:id',
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const campground = await Campground.findByIdAndDelete(id);
-		res.redirect(`/campgrounds`);
-	})
-);
-
-app.post(
-	'/campgrounds/:id/reviews',
-	validateReview,
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
-		const review = new Review(req.body.review);
-		campground.reviews.push(review);
-		await review.save();
-		await campground.save();
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-
-app.delete(
-	'/campgrounds/:id/reviews/:reviewId',
-	catchAsync(async (req, res) => {
-		const { id, reviewId } = req.params;
-		//$pull operator PULLS out of array of @reviews@ the document with @reviewId@
-		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-		await Review.findByIdAndDelete(reviewId);
-		res.redirect(`/campgrounds/${id}`);
-	})
-);
 
 //if there is no such a route (endpoint)
 app.all('*', (req, res, next) => {
