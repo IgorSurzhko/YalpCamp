f51de9f98b34b16ac990ae482f61cb0824b08dd4
Igor <ermygut@gmail.com>
2021-08-14 14:45:29 +0300

    Adding a Reviews&Users Controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,38 +1,12 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true }); //passes params (camp.id) into the router
+const reviews = require('../controllers/reviews');
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
-const Review = require('../models/review');
+
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../utils/middlewares');
 
-router.post(
-	'/',
-	isLoggedIn,
-	validateReview,
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
-		const review = new Review(req.body.review);
-		review.author = req.user._id;
-		campground.reviews.push(review);
-		await review.save();
-		await campground.save();
-		req.flash('success', 'Your review has been created successfully');
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
-router.delete(
-	'/:reviewId',
-	isLoggedIn,
-	isReviewAuthor,
-	catchAsync(async (req, res) => {
-		const { id, reviewId } = req.params;
-		//$pull operator PULLS out of array of @reviews@ the document with @reviewId@
-		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-		await Review.findByIdAndDelete(reviewId);
-		req.flash('success', 'Your review has been deleted successfully');
-		res.redirect(`/campgrounds/${id}`);
-	})
-);
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
 module.exports = router;
