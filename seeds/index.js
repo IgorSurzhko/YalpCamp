473f67123871984f00a377ccb93588e339902bc7
Igor <ermygut@gmail.com>
2021-08-16 18:21:20 +0300

    Cluster Map

--- seeds/index.js
+++ seeds/index.js
@@ -20,7 +20,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
 	await Campground.deleteMany({});
-	for (let i = 0; i < 50; i++) {
+	for (let i = 0; i < 300; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
 		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
@@ -33,7 +33,7 @@ const seedDB = async () => {
 			price,
 			geometry: {
 				type: 'Point',
-				coordinates: [-122.3301, 47.6038]
+				coordinates: [cities[random1000].longitude, cities[random1000].latitude]
 			},
 			images: [
 				{
